bd8e1b4e2d4a84201f8da2b47d6846b301073715
Jeremy Sfez <jeremy@smooth-code.com>
2024-03-27 10:48:26 +0200

    feat: setup argos

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -1,6 +1,7 @@
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
-
 import { defineConfig } from 'cypress';
+// @ts-expect-error moduleResolution
+import { registerArgosTask } from '@argos-ci/cypress/task';
 
 export default defineConfig({
   e2e: {
@@ -13,6 +14,15 @@ export default defineConfig({
       },
       ciWebServerCommand: 'nx run org:serve-static',
     }),
-    baseUrl: 'http://localhost:4200',
+    async setupNodeEvents(on, config) {
+      registerArgosTask(on, config, {
+        // Enable upload to Argos only when it runs on CI.
+        uploadToArgos: !!process.env.CI,
+        // Set your Argos token (required only if you don't use GitHub Actions).
+        token: '917a2a86b2a1edb947023ef05070839f122aff2e',
+      });
+
+      // include any other plugin code...
+    },
   },
 });
--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,13 +1,4 @@
-import { getGreeting } from '../support/app.po';
-
-describe('e2e', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
-  });
+it('Screenshot a page', () => {
+  cy.visit('https://argos-ci.com');
+  cy.argosScreenshot('homepage');
 });
